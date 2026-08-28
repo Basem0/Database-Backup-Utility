abc003f257558e12585cf240cb76eb244a5d2b91
Ahmed Basem <ahmedbasemm4@gmail.com>
2026-08-28 23:46:00 +0300

    Init Spring Boot Project

---
+++ src/test/java/com/basem/databasebackuputility/DatabaseBackupUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basem.databasebackuputility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseBackupUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
